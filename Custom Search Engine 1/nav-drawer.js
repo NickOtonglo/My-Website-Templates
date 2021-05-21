cbf81e3cf17dabb291beea232075de50e4fa1dda
crocodile <nickotonglo@gmail.com>
2021-05-21 16:39:28 +0300

    Added #filler that disables the main area when navigation drawer is open in 'Custom Search Engine 1'.

--- Custom Search Engine 1/nav-drawer.js
+++ Custom Search Engine 1/nav-drawer.js
@@ -1,14 +1,23 @@
 let openNavDrawer = document.getElementById('open-nav-drawer');
 let closeNavDrawer = document.getElementById('close-nav-drawer');
 let navDrawer = document.getElementById('nav-drawer');
+let filler = document.getElementById('filler');
 let body = document.body;
 
 openNavDrawer.onclick = function(){
     navDrawer.classList.remove('closed');
-    body.classList.add('unfocused')
+    body.classList.add('unfocused');
+    filler.classList.add('filler');
 };
 
 closeNavDrawer.onclick = function(){
     navDrawer.classList.add('closed');
     body.classList.remove('unfocused');
+    filler.classList.remove('filler');
+}
+
+filler.onclick = function(){
+    navDrawer.classList.add('closed');
+    body.classList.remove('unfocused');
+    filler.classList.remove('filler');
 }
