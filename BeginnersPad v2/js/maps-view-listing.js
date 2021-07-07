0caf8b60a3170ec34907172eb1460b16f289f867
Nick Otonglo <nickotonglo@gmail.com>
2021-07-07 11:46:42 +0300

    Added 'view-listing-entry' in 'Beginners Pad v2'.

--- BeginnersPad v2/js/maps-view-listing.js
+++ BeginnersPad v2/js/maps-view-listing.js
@@ -1,8 +1,50 @@
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+    const myLatLng = { lat: -1.3003396575224304, lng: 36.78208334575244 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 16,
+      center: myLatLng,
+    });
+    new google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: "Listing Name",
+    });
+
 }
+
+// var map;
+// var service;
+// var infowindow;
+// let obj;
+
+// function initMap() {
+//     var listingLocation = new google.maps.LatLng(-1.3003396575224304,36.78208334575244);
+
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: listingLocation,
+//         zoom: 16
+//     });
+
+//     var request = {
+//         location: listingLocation,
+//         radius: '500',
+//         query: 'restaurant'
+//     };
+
+//     service = new google.maps.places.PlacesService(map);
+//     service.textSearch(request, callback);
+// }
+
+// function callback(results, status) {
+//     if (status == google.maps.places.PlacesServiceStatus.OK) {
+//         for (var i = 0; i < 3; i++) {
+//             var place = results[i];
+//             sch = document.getElementById('sch'+(i+1)+'-title');
+//             sch.innerHtml = place.name;
+//             // createMarker(results[i]);
+//             console.log(place);
+//         }
+//     }
+// }
