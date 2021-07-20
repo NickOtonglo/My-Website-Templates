6f9042b4cd469861d2b49d7c9fba1b5960e78a1e
Nick Otonglo <nickotonglo@gmail.com>
2021-07-20 15:04:12 +0300

    Added logic to 'nav-drawer.js' for '.nav-drawer' control in 'Beginners Pad v2'.

---
+++ BeginnersPad v2/js/nav-drawer.js
@@ -0,0 +1,23 @@
+let navDrawerOpen = document.getElementById('navDrawerOpen');
+let navDrawerClose = document.getElementById('navDrawerClose');
+let navDrawer = document.getElementById('navDrawer');
+let pageFiller = document.getElementById('pageFiller');
+let body = document.body;
+
+navDrawerOpen.onclick = function(){
+    navDrawer.classList.remove('closed');
+    body.classList.add('unfocused');
+    pageFiller.classList.add('filler');
+};
+
+navDrawerClose.onclick = function(){
+    navDrawer.classList.add('closed');
+    body.classList.remove('unfocused');
+    pageFiller.classList.remove('filler');
+}
+
+pageFiller.onclick = function(){
+    navDrawer.classList.add('closed');
+    body.classList.remove('unfocused');
+    pageFiller.classList.remove('filler');
+}
