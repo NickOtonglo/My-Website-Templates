132b12e5d02116091e236116f16aba4cb1514997
Nick Otonglo <nickotonglo@gmail.com>
2021-07-22 13:19:21 +0300

    Fixed a bug that prevented '#navToggle' from closing when it contains '.open' and NOT '.authed' in 'css-base.js' in 'Beginners Pad v2'.

--- BeginnersPad v2/js/css-base.js
+++ BeginnersPad v2/js/css-base.js
@@ -5,7 +5,7 @@ let navMenu = document.getElementById('navMenu');
 navToggle.onclick = function(){
     if(navMenu.classList.contains('open')){
         navMenu.classList.remove('open');
-    } if(navMenu.classList.contains('authed')){
+    } else if(navMenu.classList.contains('authed')){
         navDrawer.classList.remove('closed');
     } else {
         navMenu.classList.add('open');
