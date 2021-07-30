43fefbadb981532230a6f6e4d45d9cfeaadabc48
Nick Otonglo <nickotonglo@gmail.com>
2021-07-30 18:27:32 +0300

    Temporarily removed references to 'popup.js' and restored 'nav-drawer.js' original functionality in 'Beginners Pad v2'.

--- BeginnersPad v2/js/nav-drawer.js
+++ BeginnersPad v2/js/nav-drawer.js
@@ -1,34 +1,58 @@
 let navDrawerOpen = document.getElementById('navDrawerOpen');
 let navDrawerClose = document.getElementById('navDrawerClose');
 let navDrawer = document.getElementById('navDrawer');
-
+let pageFiller = document.getElementById('pageFiller');
+let body = document.body;
 
 navDrawerOpen.onclick = function(){
-    if(popupDemo.classList.contains('closed')){
-        navDrawer.classList.remove('closed');
-        body.classList.add('unfocused');
-        pageFiller.classList.add('filler');
-    } else {
-        navDrawer.classList.remove('closed');
-    }
+    navDrawer.classList.remove('closed');
+    body.classList.add('unfocused');
+    pageFiller.classList.add('filler');
 };
 
 navDrawerClose.onclick = function(){
-    if(popupDemo.classList.contains('closed')){
-        navDrawer.classList.add('closed');
-        body.classList.remove('unfocused');
-        pageFiller.classList.remove('filler');
-    } else {
-        navDrawer.classList.add('closed');
-    }
+    navDrawer.classList.add('closed');
+    body.classList.remove('unfocused');
+    pageFiller.classList.remove('filler');
 }
 
 pageFiller.onclick = function(){
-    if(popupDemo.classList.contains('closed')){
-        navDrawer.classList.add('closed');
-        body.classList.remove('unfocused');
-        pageFiller.classList.remove('filler');
-    } else {
-        navDrawer.classList.add('closed');
-    }
+    navDrawer.classList.add('closed');
+    body.classList.remove('unfocused');
+    pageFiller.classList.remove('filler');
 }
+
+// let navDrawerOpen = document.getElementById('navDrawerOpen');
+// let navDrawerClose = document.getElementById('navDrawerClose');
+// let navDrawer = document.getElementById('navDrawer');
+
+
+// navDrawerOpen.onclick = function(){
+//     if(popupDemo.classList.contains('closed')){
+//         navDrawer.classList.remove('closed');
+//         body.classList.add('unfocused');
+//         pageFiller.classList.add('filler');
+//     } else {
+//         navDrawer.classList.remove('closed');
+//     }
+// };
+
+// navDrawerClose.onclick = function(){
+//     if(popupDemo.classList.contains('closed')){
+//         navDrawer.classList.add('closed');
+//         body.classList.remove('unfocused');
+//         pageFiller.classList.remove('filler');
+//     } else {
+//         navDrawer.classList.add('closed');
+//     }
+// }
+
+// pageFiller.onclick = function(){
+//     if(popupDemo.classList.contains('closed')){
+//         navDrawer.classList.add('closed');
+//         body.classList.remove('unfocused');
+//         pageFiller.classList.remove('filler');
+//     } else {
+//         navDrawer.classList.add('closed');
+//     }
+// }
