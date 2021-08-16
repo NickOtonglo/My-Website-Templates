ab4b44c1a32fb32c3a3a3242f71f81f7f788637c
Nick Otonglo <nickotonglo@gmail.com>
2021-08-16 20:55:00 +0300

    Added logic to 'charts-lister-dashboard.js' in 'Beginners Pad v2'.

---
+++ BeginnersPad v2/js/charts-lister-dashboard.js
@@ -0,0 +1,241 @@
+
+var chartDashListerListings = document.getElementById('chartDashListerListings');
+var chartDashListerUnits = document.getElementById('chartDashListerUnits');
+var chartDashListerBegAppl = document.getElementById('chartDashListerBegAppl');
+var chartDashListerTours = document.getElementById('chartDashListerTours');
+var chartDashListerBanned = document.getElementById('chartDashListerBanned');
+var chartDashListerRating = document.getElementById('chartDashListerRating');
+var chartDashListerTickets = document.getElementById('chartDashListerTickets');
+var chartDashListerFavs = document.getElementById('chartDashListerFavs');
+
+var chartListerListings = new Chart(chartDashListerListings, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartListerUnits = new Chart(chartDashListerUnits, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartListerBegAppl = new Chart(chartDashListerBegAppl, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartListerTours = new Chart(chartDashListerTours, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartListerBanned = new Chart(chartDashListerBanned, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartListerRating = new Chart(chartDashListerRating, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartListerTickets = new Chart(chartDashListerTickets, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartListerFavs = new Chart(chartDashListerFavs, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
