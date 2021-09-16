8649597d4c8fade35583b9dbae55e195bd2c6811
Nick Otonglo <nickotonglo@gmail.com>
2021-09-16 18:46:52 +0300

    Added JavaScript logic for '.btn-dropdown-grp' in 'css-base.js' in 'Beginners Pad v2'.

--- BeginnersPad v2/js/css-base.js
+++ BeginnersPad v2/js/css-base.js
@@ -1,6 +1,15 @@
 // Nav menu toggle
 let navToggle = document.getElementById('navToggle');
 let navMenu = document.getElementById('navMenu');
+let btnDropGrp1,btn_dropdown_grp,listDropGrp1;
+
+if (document.getElementById('btnDropGrp1')){
+    btnDropGrp1 = document.getElementById('btnDropGrp1');
+    listDropGrp1 = document.getElementById('listDropGrp1');
+    // console.log('exists');
+} else {
+    // console.log('!exists');
+}
 
 navToggle.onclick = function(){
     if(navMenu.classList.contains('open')){
@@ -13,3 +22,11 @@ navToggle.onclick = function(){
         navMenu.classList.add('open');
     }
 };
+
+btnDropGrp1.onclick = function(){
+    if(listDropGrp1.classList.contains('open')){
+        listDropGrp1.classList.remove('open');
+    } else {
+        listDropGrp1.classList.add('open');
+    }
+}
