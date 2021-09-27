908e0dc63c27a70b4e6d9393a90b0f96a3eba03b
Nick Otonglo <nickotonglo@gmail.com>
2021-09-27 18:07:11 +0300

    Added logic to '/js/charts-admin-dashboard.html' in 'Beginners Pad v2'.

---
+++ BeginnersPad v2/js/charts-admin-dashboard.js
@@ -0,0 +1,361 @@
+
+var chartDashAdminUsers = document.getElementById('chartDashAdminUsers');
+var chartDashAdminListings = document.getElementById('chartDashAdminListings');
+var chartDashAdminUnits = document.getElementById('chartDashAdminUnits');
+var chartDashAdminBegAppl = document.getElementById('chartDashAdminBegAppl');
+var chartDashAdminReviews = document.getElementById('chartDashAdminReviews');
+var chartDashAdminTours = document.getElementById('chartDashAdminTours');
+var chartDashAdminRating = document.getElementById('chartDashAdminRating');
+var chartDashAdminTickets = document.getElementById('chartDashAdminTickets');
+var chartDashAdminZones = document.getElementById('chartDashAdminZones');
+var chartDashAdminTopics = document.getElementById('chartDashAdminTopics');
+var chartDashAdminTags = document.getElementById('chartDashAdminTags');
+var chartDashAdminFavs = document.getElementById('chartDashAdminFavs');
+
+var chartAdminUsers = new Chart(chartDashAdminUsers, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminListings = new Chart(chartDashAdminListings, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminUnits = new Chart(chartDashAdminUnits, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminBegAppl = new Chart(chartDashAdminBegAppl, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminReviews = new Chart(chartDashAdminReviews, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminTours = new Chart(chartDashAdminTours, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminRating = new Chart(chartDashAdminRating, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminTickets = new Chart(chartDashAdminTickets, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminZones = new Chart(chartDashAdminZones, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminTopics = new Chart(chartDashAdminTopics, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminTags = new Chart(chartDashAdminTags, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var chartAdminFavs = new Chart(chartDashAdminFavs, {
+    type: 'bar',
+    data: {
+        labels: ['listings', 'units', 'topics'],
+        datasets: [{
+            label: 'Favourites',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderColor: [
+                '#703680',
+                '#362c41',
+                '#a058f1'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
