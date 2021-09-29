3f9faaa538a4298abb188448a0d356745dd3e7a3
Nick Otonglo <nickotonglo@gmail.com>
2021-09-29 06:41:10 +0300

    Added JS logic for '.modal' in '/manage-account.html' in 'Beginners Pad v2'.

--- BeginnersPad v2/js/css-base.js
+++ BeginnersPad v2/js/css-base.js
@@ -1,12 +1,47 @@
 // Nav menu toggle
 let navToggle = document.getElementById('navToggle');
 let navMenu = document.getElementById('navMenu');
-let btnDropGrp1,btn_dropdown_grp,listDropGrp1;
+let body = document.body;
+let btnDropGrp1,btn_dropdown_grp,listDropGrp1,pageFiller,
+modalTrigger,modal,modalHeaderClose,modalFooterClose;
+
+if (document.getElementById('modalTrigger') && document.getElementById('pageFiller')){
+    modalTrigger = document.getElementById('modalTrigger');
+    modal = document.getElementById('modal');
+    modalFooterClose = document.getElementById('modalFooterClose');
+    modalHeaderClose = document.getElementById('modalHeaderClose');
+    pageFiller = document.getElementById('pageFiller');
+    modalTrigger.onclick = function(){
+        toggleModal();
+    }
+    pageFiller.onclick = function(){
+        toggleModal();
+    }
+    modalHeaderClose.onclick = function(){
+        toggleModal();
+    }
+    modalFooterClose.onclick = function(){
+        toggleModal();
+    }
+}
 
 if (document.getElementById('btnDropGrp1')){
     btnDropGrp1 = document.getElementById('btnDropGrp1');
     listDropGrp1 = document.getElementById('listDropGrp1');
     // console.log('exists');
+    btnDropGrp1.onclick = function(){
+        if(listDropGrp1.classList.contains('open')){
+            listDropGrp1.classList.remove('open');
+        } else {
+            listDropGrp1.classList.add('open');
+        }
+    }
+    
+    listDropGrp1.onclick = function(){
+        if(listDropGrp1.classList.contains('open')){
+            listDropGrp1.classList.remove('open');
+        }
+    }
 } else {
     // console.log('!exists');
 }
@@ -23,16 +58,14 @@ navToggle.onclick = function(){
     }
 };
 
-btnDropGrp1.onclick = function(){
-    if(listDropGrp1.classList.contains('open')){
-        listDropGrp1.classList.remove('open');
+function toggleModal() {
+    if(modal.classList.contains('open')){
+        modal.classList.remove('open');
+        body.classList.remove('unfocused');
+        pageFiller.classList.remove('filler');
     } else {
-        listDropGrp1.classList.add('open');
-    }
-}
-
-listDropGrp1.onclick = function(){
-    if(listDropGrp1.classList.contains('open')){
-        listDropGrp1.classList.remove('open');
+        modal.classList.add('open');
+        body.classList.add('unfocused');
+        pageFiller.classList.add('filler');
     }
 }
