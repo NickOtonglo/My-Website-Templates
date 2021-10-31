1b0b14ba686fa7871a42ef9a9963997581b96d10
Nick Otonglo <nickotonglo@gmail.com>
2021-10-31 19:16:05 +0300

    Added marker functionality for nearby places and marker toggle on place click to 'js/maps-view-listing.js in 'Beginners Pad v2'.

--- BeginnersPad v2/js/maps-view-listing.js
+++ BeginnersPad v2/js/maps-view-listing.js
@@ -34,19 +34,19 @@ var infowindow;
 let request;
 let sch,mkt,bus;
 let marker1, marker2;
-let marker2Lat,marker2Lng;
-let kilimani;
+let markers = [];
+let listingPlace;
 
 function initPlaces() {
-  kilimani = new google.maps.LatLng(-1.3003396575224304, 36.78208334575244);
+  listingPlace = new google.maps.LatLng(-1.3003396575224304, 36.78208334575244);
 
   map = new google.maps.Map(document.getElementById('map'), {
-      center: kilimani,
+      center: listingPlace,
       zoom: 15
     });
 
   request = {
-    location: kilimani,
+    location: listingPlace,
     radius: '500',
     type: ['school'],
     rankBy: google.maps.places.RankBy.PROMINENCE
@@ -56,7 +56,7 @@ function initPlaces() {
   service.nearbySearch(request, callback);
 
   request = {
-    location: kilimani,
+    location: listingPlace,
     radius: '500',
     type: ['supermarket'],
     rankBy: google.maps.places.RankBy.PROMINENCE
@@ -66,7 +66,7 @@ function initPlaces() {
   service.nearbySearch(request, callback);
 
   request = {
-    location: kilimani,
+    location: listingPlace,
     radius: '500',
     type: ['bus_station'],
     rankBy: google.maps.places.RankBy.PROMINENCE
@@ -74,28 +74,12 @@ function initPlaces() {
 
   service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
-
-  marker1 = new google.maps.Marker({
-    position: { lat: -1.3003396575224304, lng: 36.78208334575244 },
-    map: map,
-  });
-}
-
-function setPlacesDist(){
-  console.log(google.maps.geometry.spherical
-    .computeDistanceBetween(kilimani, new google.maps.LatLng(marker2Lat, marker2Lng)));
 }
 
 function callback(results, status) {
+  const placesObject = {};
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      // new google.maps.Marker({
-      //   map: map,
-      //   place: {
-      //       placeId: results[i].place_id,
-      //       location: results[i].geometry.location
-      //   }
-      // });
       
       if(i+1 <= 3){
         if(results[i].types[0].includes('school')){
@@ -103,87 +87,122 @@ function callback(results, status) {
           sch.innerHTML = results[i].name;
           document.querySelector(`#sch${i+1} .description`).innerHTML = `Status: ${results[i].business_status}`;
           document.querySelector(`#sch${i+1} .location`).innerHTML = results[i].vicinity;
+          if(i === 0){
+            placesObject.id_sch1 = i;
+            placesObject.data_sch1 = results[i];
+            let sch1 = document.querySelector('#sch1');
+            sch1.onclick = () => {
+              placeLocationMarker(placesObject.data_sch1);
+            }
+          }
+          if(i === 1){
+            placesObject.id_sch2 = i;
+            placesObject.data_sch2 = results[i];
+            let sch2 = document.querySelector('#sch2');
+            sch2.onclick = () => {
+              placeLocationMarker(placesObject.data_sch2);
+            }
+          }
+          if(i === 2){
+            placesObject.id_sch3 = i;
+            placesObject.data_sch3 = results[i];
+            let sch3 = document.querySelector('#sch3');
+            sch3.onclick = () => {
+              placeLocationMarker(placesObject.data_sch3);
+            }
+          }
         }
         if(results[i].types[0].includes('market')){
           mkt = document.querySelector(`#mkt${i+1} .title`);
           mkt.innerHTML = results[i].name;
           document.querySelector(`#mkt${i+1} .description`).innerHTML = `Status: ${results[i].business_status}`;
           document.querySelector(`#mkt${i+1} .location`).innerHTML = results[i].vicinity;
+          if(i === 0){
+            placesObject.id_mkt1 = i;
+            placesObject.data_mkt1 = results[i];
+            let mkt1 = document.querySelector('#mkt1');
+            mkt1.onclick = () => {
+              placeLocationMarker(placesObject.data_mkt1);
+            }
+          }
+          if(i === 1){
+            placesObject.id_mkt2 = i;
+            placesObject.data_mkt2 = results[i];
+            let mkt2 = document.querySelector('#mkt2');
+            mkt2.onclick = () => {
+              placeLocationMarker(placesObject.data_mkt2);
+            }
+          }
+          if(i === 2){
+            placesObject.id_mkt3 = i;
+            placesObject.data_mkt3 = results[i];
+            let mkt3 = document.querySelector('#mkt3');
+            mkt3.onclick = () => {
+              placeLocationMarker(placesObject.data_mkt3);
+            }
+          }
         }
         if(results[i].types[0].includes('bus_station')){
           bus = document.querySelector(`#bus${i+1} .title`);
           bus.innerHTML = results[i].name;
           document.querySelector(`#bus${i+1} .description`).innerHTML = `Status: ${results[i].business_status}`;
           document.querySelector(`#bus${i+1} .location`).innerHTML = results[i].vicinity;
-
-          var service = new google.maps.places.PlacesService(map);
-          service.getDetails({
-              placeId: results[i].place_id
-          }, function (result, status) {
-              marker2 = new google.maps.Marker({
-                  map: map,
-                  place: {
-                      placeId: result.place_id,
-                      location: result.geometry.location
-                  }
-              });
-              console.log(marker2);
-          });
-
-          // console.log(marker1.getPosition().lat());
-
-          // const app = async () => {
-          //   const response = await marker2.getPosition().lat();
-          //   console.log(response.results);
-          // };
-          
-          // app();
-
-          // getCoordinates(marker2.getPosition().lat(),marker2.getPosition().lng(),
-          // () => {console.log(google.maps.geometry.spherical
-          //     .computeDistanceBetween(kilimani, new google.maps.LatLng(marker2Lat, marker2Lng)))})
-
-          // marker2Lat = marker2.getPosition().lat();
-          // marker2Lng = marker2.getPosition().lng();
-          // console.log(google.maps.geometry.spherical
-          //   .computeDistanceBetween(kilimani, new google.maps.LatLng(marker2Lat, marker2Lng)));
+          if(i === 0){
+            placesObject.id_bus1 = i;
+            placesObject.data_bus1 = results[i];
+            let bus1 = document.querySelector('#bus1');
+            bus1.onclick = () => {
+              placeLocationMarker(placesObject.data_bus1);
+            }
+          }
+          if(i === 1){
+            placesObject.id_bus2 = i;
+            placesObject.data_bus2 = results[i];
+            let bus2 = document.querySelector('#bus2');
+            bus2.onclick = () => {
+              placeLocationMarker(placesObject.data_bus2);
+            }
+          }
+          if(i === 2){
+            placesObject.id_bus3 = i;
+            placesObject.data_bus3 = results[i];
+            let bus3 = document.querySelector('#bus3');
+            bus3.onclick = () => {
+              placeLocationMarker(placesObject.data_bus3);
+            }
+          }
         }
       }
     }
   }
 }
 
-// var map;
-// var service;
-// var infowindow;
-// let obj;
+function placeLocationMarker(place){
+  document.querySelector('#map-view').scrollIntoView();
+  hideMarkers();
+  var service = new google.maps.places.PlacesService(map);
+  service.getDetails({
+    placeId: place.place_id
+  }, (result, status) => {
+    marker2 = new google.maps.Marker({
+      map: map,
+      place: {
+        placeId: result.place_id,
+        location: result.geometry.location
+      }
+    });
+    markers.push(marker2);
+  });
+  // console.log(markers);
+}
 
-// function initMap() {
-//     var listingLocation = new google.maps.LatLng(-1.3003396575224304,36.78208334575244);
+function setMapOnAll(map) {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
 
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: listingLocation,
-//         zoom: 16
-//     });
-
-//     var request = {
-//         location: listingLocation,
-//         radius: '500',
-//         query: 'restaurant'
-//     };
-
-//     service = new google.maps.places.PlacesService(map);
-//     service.textSearch(request, callback);
-// }
-
-// function callback(results, status) {
-//     if (status == google.maps.places.PlacesServiceStatus.OK) {
-//         for (var i = 0; i < 3; i++) {
-//             var place = results[i];
-//             sch = document.getElementById('sch'+(i+1)+'-title');
-//             sch.innerHtml = place.name;
-//             // createMarker(results[i]);
-//             console.log(place);
-//         }
-//     }
-// }
+function hideMarkers() {
+  setMapOnAll(null);
+  markers = [];
+}
