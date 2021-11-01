ed22b6940678e8da5af00c9498148fb799987a4f
Nick Otonglo <nickotonglo@gmail.com>
2021-11-01 16:21:40 +0300

    Added logic to '/js/maps-zones.js', changed map script for '/admin/manage-zone.html' to '/js/map-zones.js', and set on click listener to '#mapSubzone' to update marker on map as well as '#latitude' and '#longitude' fields in 'Beginners Pad v2'.

---
+++ BeginnersPad v2/js/maps-zones.js
@@ -0,0 +1,56 @@
+let map;
+let markerZone,latZone,lngZone;
+
+function initZone() {
+  const myLatLng = { lat: -1.3003396575224304, lng: 36.78208334575244 };
+
+  if(document.querySelector("#map")){
+    const map = new google.maps.Map(document.querySelector("#map"), {
+      zoom: 14,
+      center: myLatLng,
+      mapTypeControl: false,
+    });
+    new google.maps.Circle({
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+        map,
+        center: myLatLng,
+        radius: 1000,
+    });
+  }
+
+  if(document.querySelector("#mapSubzone")){
+    const map2 = new google.maps.Map(document.querySelector("#mapSubzone"), {
+      zoom: 15,
+      center: myLatLng,
+      mapTypeControl: false,
+    });
+    // new google.maps.Marker({
+    //   position: myLatLng,
+    //   map: map2,
+    //   title: "Sub-zone",
+    // });
+    google.maps.event.addListener(map2,'click',(event) => {
+        placeMarker(event.latLng,map2);
+        document.querySelector('#latitude').value = latZone;
+        document.querySelector('#longitude').value = lngZone;
+    });
+  }
+}
+
+function placeMarker(location, map) {
+    if (markerZone == null) {
+        markerZone = new google.maps.Marker({
+            position: location,
+            map: map,
+            title: "Centre of sub-zone",
+        });
+    } else {
+        markerZone.setPosition(location);
+    }
+    latZone = markerZone.getPosition().lat();
+    lngZone = markerZone.getPosition().lng();
+}
